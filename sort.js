98a63d51a4d5091252da3307caa2f0fdf30a0ed3
Anmensah <141930241+Anmensah@users.noreply.github.com>
2025-02-06 10:01:32 +0000

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,155 @@
+let breakingImg = document.querySelector('#breakingImg');
+let breakingNews_title = document.querySelector('#breakingNews .title');
+let breakingNews_desc = document.querySelector('#breakingNews .description');
+let topNews = document.querySelector('.topNews');
+let sportsNews = document.querySelector('#sportsNews .newsBox');
+let businessNews = document.querySelector('#businessNews .newsBox');
+let techNews = document.querySelector('#techNews .newsBox');
+
+let header = document.querySelector('.header');
+let toggleMenu = document.querySelector('.bar');
+let menuItems = document.querySelectorAll('nav li'); // Select all <li> elements
+
+const toggle = (e) => {
+    toggleMenu.classList.toggle('active');
+    menuItems.forEach(menuItem => {
+        menuItem.classList.toggle('activeMenu');
+    });
+};
+
+toggleMenu.addEventListener('click', toggle);
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
+// Fetching news data from a website providing API.
+const apiKey = "ed550cf263a74984abe9b1c66873635d";
+
+const fetchData = async (category, pageSize) => {
+    try {
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`;
+        const data = await fetch(url);
+        const response = await data.json();
+        console.log(response);
+        return response.articles; // Return response.articles instead of data.articles
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+const add_breakingNews = (data) => {
+    breakingImg.innerHTML = `<img src=${data[0].urlToImage} alt="image">`;
+    breakingNews_title.innerHTML = `<a href=${data[0].url} target="_blank"><h2>${data[0].title}</h2></a>`;
+    breakingNews_desc.innerHTML = `<h2>${data[0].description}</h2>`;
+};
+
+fetchData('general', 5).then(add_breakingNews);
+
+const add_topNews = (data) => {
+    let html = '';
+    let title = '';
+    data.forEach((element) => {
+        if (element.title.length < 100) {
+            title = element.title;
+        } else {
+            title = element.title.slice(0, 100) + "...";
+        }
+
+        html += `<div class="news">
+                    <div class="img">
+                        <img src=${element.urlToImage} alt="image">
+                    </div>
+                    <div class="text">
+                        <div class="title">
+                            <a href=${element.url} target="_blank"><p>${title}</p></a>
+                        </div>
+                    </div>
+                </div>`;
+    });
+    topNews.innerHTML = html;
+};
+
+fetchData('general', 20).then(add_topNews);
+
+const add_sportsNews = (data) => {
+    let html = '';
+    let title = '';
+    data.forEach((element) => {
+        if (element.title.length < 100) {
+            title = element.title;
+        } else {
+            title = element.title.slice(0, 100) + "...";
+        }
+
+        html += `<div class="newsCard">
+                    <div class="img">
+                        <img src=${element.urlToImage} alt="image">
+                    </div>
+                    <div class="text">
+                        <div class="title">
+                            <a href=${element.url} target="_blank"><p>${title}</p></a>
+                        </div>
+                    </div>
+                </div>`;
+    });
+    sportsNews.innerHTML = html;
+};
+
+fetchData('sports', 5).then(add_sportsNews);
+
+const add_businessNews = (data) => {
+    let html = '';
+    let title = '';
+    data.forEach((element) => {
+        if (element.title.length < 100) {
+            title = element.title;
+        } else {
+            title = element.title.slice(0, 100) + "...";
+        }
+
+        html += `<div class="newsCard">
+                    <div class="img">
+                        <img src=${element.urlToImage} alt="image">
+                    </div>
+                    <div class="text">
+                        <div class="title">
+                            <a href=${element.url} target="_blank"><p>${title}</p></a>
+                        </div>
+                    </div>
+                </div>`;
+    });
+    businessNews.innerHTML = html;
+};
+
+fetchData('business', 5).then(add_businessNews);
+
+const add_technews = (data) => {
+    let html = '';
+    let title = '';
+    data.forEach((element) => {
+        if (element.title.length < 100) {
+            title = element.title;
+        } else {
+            title = element.title.slice(0, 100) + "...";
+        }
+
+        html += `<div class="newsCard">
+                    <div class="img">
+                        <img src=${element.urlToImage} alt="image">
+                    </div>
+                    <div class="text">
+                        <div class="title">
+                            <a href=${element.url} target="_blank"><p>${title}</p></a>
+                        </div>
+                    </div>
+                </div>`;
+    });
+    techNews.innerHTML = html;
+};
+
+fetchData('technology', 5).then(add_technews); // Corrected function name
